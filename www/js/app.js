f9e00aa499e4efdbdea484cd8889e0c267acf372
Jeff <fenster.js@gmail.com>
2016-05-13 13:03:36 -0400

    Added Sticker Functionality

--- www/js/app.js
+++ www/js/app.js
@@ -6,6 +6,7 @@
 // var base = 'http://192.168.1.7:1337'
 // 127.0.0.1 dynamically routes to your local IP.
 var base = 'http://127.0.0.1:27017:1337'
+var base = 'http://192.168.1.183:1337'
 
 // Ionic Starter App
 
@@ -14,7 +15,7 @@ var base = 'http://127.0.0.1:27017:1337'
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-var core = angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'fsaPreBuilt', 'ngCordova', 'ngStorage'])
+var core = angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'fsaPreBuilt', 'ngCordova', 'ngStorage', 'hmTouchEvents' ])
 
 
 core.run(function($ionicPlatform, $rootScope, $state) {
@@ -63,6 +64,9 @@ core.config(function($stateProvider, $urlRouterProvider) {
     resolve: {
       story: function(StoryFactory, $stateParams) {
         return StoryFactory.getStoryById($stateParams.storyId);
+      },
+      getAddons: function(CameraFactory, $stateParams) {
+        return CameraFactory.getFilters()
       }
     }
   })
@@ -93,16 +97,6 @@ core.config(function($stateProvider, $urlRouterProvider) {
     resolve: {
       story: function(StoryFactory, $stateParams) {
         return StoryFactory.getStoryById($stateParams.storyId);
-      }
-    }
-  })
-  .state('testState', {
-    url: '/testState',
-    templateUrl: 'js/testState/testState.template.html',
-    controller: 'testStateCtrl',
-    resolve: {
-      getAddons: function(TestFactory, $stateParams) {
-        return TestFactory.getFilters()
       }
     }
   })
--- www/js/camera/camera.controller.js
+++ www/js/camera/camera.controller.js
@@ -1,9 +1,14 @@
-core.controller('CameraCtrl', function(story, $scope, $cordovaCamera, $cordovaFileTransfer, Grafi, $localStorage, CameraFactory) {
-	$scope.story = story;
+core.controller('CameraCtrl', function(story, getAddons, $scope, $cordovaCamera, $cordovaFileTransfer, Grafi, $localStorage, CameraFactory) {
+	
+    $scope.story = story;
     console.log('current story: ', $scope.story)
 
 
+
+
+
     $scope.takePicture = function() {
+        console.log("THE CAMERA RAN ON THE ISOLATE SCOPE")
         var options = { 
             quality : 75, 
             destinationType : Camera.DestinationType.DATA_URL, 
@@ -25,7 +30,7 @@ core.controller('CameraCtrl', function(story, $scope, $cordovaCamera, $cordovaFi
     }
 
     $scope.openPhotoLibrary = function() { 
-        console.log('in open photo library')
+        console.log('in open photo library', $cordovaCamera)
         var options = {
             quality: 50,
             destinationType: Camera.DestinationType.FILE_URI,
@@ -73,42 +78,75 @@ core.controller('CameraCtrl', function(story, $scope, $cordovaCamera, $cordovaFi
         })
     }
 
+    //--------DIRECTIVE--------//
+
+    $scope.test = function(){
+        console.log("HELLO")
+    }
+    //Filters from Database Resolve
+    $scope.allAddons = getAddons
+    console.log($scope.allAddons)
+
+    //Create Sticker Div
+ 
+    // var w = document.getElementById('stickerWrapper');
+
+    // w.style.left = '50px';
+    // w.style.top = '100px'
+
+    $scope.stickersArray = []
+    stickercounter = 1;
+    $scope.sticker = function (img){
+        console.log('STICKER', img)
+
+        //Create image element with unique ID
+        if(stickercounter < 4){
+            // var sticker = new Image()
+            // sticker.src = img
+            // console.log(sticker)
+
+            // sticker.setAttribute("src", img)
+            // sticker.setAttribute("hm-panmove", 'onHammer')
+            // sticker.setAttribute("ng-click", "test()")
+            $scope.stickersArray.push({source: img, id: stickercounter})
+
+            console.log($scope.stickersArray)
+            //Grab that element and set it to a variable;
+            // w.appendChild(sticker)
+            stickercounter++
+        } else {
+            //Run an Error that tells them they have too many stickers!
+            console.log("Too Many Stickers!")
+        }
+        // $scope.$compile()
+    }    
+    $scope.bubble = function (img){
+        console.log('BUBBLE')
+    }    
+    $scope.border = function (img){
+        console.log('BORDER')
+    }    
+    $scope.filter = function (img){
+        console.log('FILTER')
+    }
+
+    $scope.onHammer = function onHammer (event) {
+
+        var currentElem = document.getElementById(event.element[0].id);
+
+        var x = event.center.x - 80,
+            y = event.center.y - 130;
+
+        currentElem.style.left = x + 'px';
+        currentElem.style.top = y + 'px';
+        console.log("hammer ran", currentElem)
+
+      console.log("Coords", x, y);
+
+    };
+
+
 });
 
 
-    // var context = document.getElementById('one').getContext('2d');
-    // var img = new Image();
-    // img.onload = function(){
-    //     context.drawImage(img, 0, 0);
-    // }
-    // img.src = '/Users/Debanshi/comic-book/server/app/assets/5733a34ec85339ef0b76ea56'
-
-
-                // function dataURLToBlob(dataURL) {
-                //     var BASE64_MARKER = ';base64,';
-                //     if (dataURL.indexOf(BASE64_MARKER) == -1) {
-                //       var parts = dataURL.split(',');
-                //       var contentType = parts[0].split(':')[1];
-                //       var raw = decodeURIComponent(parts[1]);
-
-                //       return new Blob([raw], {type: contentType});
-                //     }
-
-                //     var parts = dataURL.split(BASE64_MARKER);
-                //     var contentType = parts[0].split(':')[1];
-                //     var raw = window.atob(parts[1]);
-                //     var rawLength = raw.length;
-
-                //     var uInt8Array = new Uint8Array(rawLength);
-
-                //     for (var i = 0; i < rawLength; ++i) {
-                //       uInt8Array[i] = raw.charCodeAt(i);
-                //     }
-
-                //     return new Blob([uInt8Array], {type: contentType});
-                // }
-
-                // var x = dataURLToBlob(dataURL);
-                // console.log('dataURL to blob', x)
-                // var url = URL.createObjectURL(x);
-                // console.log('url from create object url', url)
+ 
