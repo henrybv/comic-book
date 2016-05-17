970d6fbf3eef4ad69522cf1fb80d63b482c6f43b
Katherine Mello <katherine.mello8@gmail.com>
2016-05-17 15:12:12 -0400

    restores code review files

---
+++ server/app/routes/stories/index.js
@@ -0,0 +1,125 @@
+'use strict';
+var router = require('express').Router();
+var mongoose = require('mongoose');
+var Story = mongoose.model('Story');
+var Promise = require('bluebird');
+var fs = require('fs');
+var path = require('path');
+module.exports = router;
+
+//get all stories
+router.get('/', function(req, res, next) {
+    Story.find({}).exec()
+    .then(function(stories) {
+        res.status(200).send(stories);
+    })
+    .catch(next);
+});
+
+//TEST THIS
+//get a specific story and all its squares
+router.get('/:storyId', function(req, res, next) {
+    Story.findById(req.params.storyId)
+    .populate('squares friends')
+    .exec()
+    .then(function(story) {
+        console.log('get story from route', story)
+        res.status(200).send(story);
+    })
+    .catch(next)
+});
+
+//get all stories created by a user
+router.get('/user/:userId', function(req, res, next) {
+    Story.find({owner: req.params.userId})
+    .populate('friends')
+    .exec()
+    .then(function(stories) {
+        res.status(200).send(stories);
+    })
+    .catch(next)
+});
+
+// get all stories a user is a collaborator on
+router.get('/collaborator/:userId', function(req, res, next) {
+    Story.find({friends: req.params.userId})
+    .populate('owner')
+    .exec()
+    .then(function(stories) {
+        res.status(200).send(stories);
+    })
+    .catch(next)
+});
+
+router.post('/', function(req, res, next) {
+    Story.create(req.body)
+    .then(function(story) {
+        return  Story.findById(story._id).populate('friends');
+    })
+    .then(function(story) {
+        res.send(story);
+    })
+    .catch(next)
+});
+
+//create new square AND update story with a new square
+router.put('/:storyId/squares', function(req, res, next){
+    var SQUARETOSEND;
+    return mongoose.model('Square').create({creator: req.body.creator})
+    .then(function(newSquare){
+        // console.log('NEW SQUARE: ', newSquare);
+        // var writeToPath = path.join(__dirname, '../../assets/' + newSquare._id);
+        // fs.writeFile(writeToPath, req.body.dataUrl, function(err) {
+            // console.log('hit callback');
+        // })
+        return mongoose.model('Square')
+        .findByIdAndUpdate(newSquare._id, {upsert: true, new: true})
+    })
+    .then(function(square) {
+        SQUARETOSEND = square;
+        console.log('SQUARE WITH SRC PATH AS FINAL IMAGE: ', square)
+        return Story.findByIdAndUpdate(req.params.storyId, {$push: {'squares': square._id}}, { upsert: true, new: true });
+    })
+    .then(function(){
+        console.log('still access to SQUARE', SQUARETOSEND)
+        res.status(200).send(SQUARETOSEND);
+    })
+    .catch(next)
+});
+
+router.put('/:storyId/collaborators', function(req, res, next) {
+    Story.update({ _id: req.params.storyId },{ $pushAll: { friends: req.body.collaborators }},{ upsert: true, new: true })
+    .then(function(story) {
+        console.log('UPDATED STORY: ', story);
+        res.send(story);
+    })
+    .catch(next);
+});
+
+
+
+
+
+
+
+    // function decodeBase64Image(dataString) {
+    //   var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+    //   var response = {};
+    //   if (matches.length !== 3) {
+    //     return new Error('Invalid input string');
+    //   }
+    //   response.type = matches[1];
+    //   response.data = new Buffer(matches[2], 'base64');
+    //   return response;
+    // }
+    // var imageBuffer = decodeBase64Image(req.body.dataUrl);
+    // console.log('IMAGE BUFFER: ', imageBuffer);
+
+
+
+
+
+
+
+
+
---
+++ www/js/story/story.controller.js
@@ -0,0 +1,104 @@
+core.controller('StoryCtrl', function($scope, story, $state, $localStorage, CameraFactory, loggedInUser, allUsers, StoryFactory, $rootScope) {
+    $scope.allUsers = allUsers;
+    $scope.currentUser = loggedInUser;
+    $scope.allUsers = allUsers;
+    $scope.clicked = false;
+    $scope.collaborators = [];
+    $scope.collabAdded = false;
+	$scope.story = story;
+    // $scope.urlbaby;
+	console.log('story in storyCTRL', $scope.story)
+
+    // $scope.allUsers.forEach(function(user) {
+    //     for (var i = 0; i < $scope.story.friends.length; i++) {
+    //         if ($scope.story.friends[i]._id === user._id) {
+    //             $scope.allUsers.splice(i, 1);
+    //         }
+    //     }
+    // });
+
+    $scope.goToCamera = function(){
+        $state.go('camera', {storyId: $scope.story._id})
+    }
+
+    $scope.changeState = function() {
+        $state.go('home')
+    }
+
+
+    var urlToNewCanvas = function(url, canvasId){
+    	var canvas = document.createElement('canvas');
+        canvas.id = canvasId;
+        canvas.width = canvas.height = 115;
+        var context = canvas.getContext('2d');
+        var newImage = new Image();
+        var elem = document.getElementById('here')
+        elem.appendChild(canvas);
+        newImage.src = url;
+        newImage.onload = function(){
+            context.drawImage(newImage, 0, 0, newImage.width, newImage.height, 0, 0, canvas.width, canvas.height);
+        }
+    }
+
+    
+// GETTING IMAGES FROM FIREBASE EVERY TIME ONE IS ADDED
+    var ref = new Firebase('https://torrid-inferno-1552.firebaseio.com/' + $scope.story._id);
+    ref.on('value', function(snapshot){
+        var here = document.getElementById('here');
+            console.log('HERE I AM', here)
+        while (here.firstChild){
+            console.log('HERE FIRST CHILD', here.firstChild)
+            here.removeChild(here.firstChild);
+        }
+        var obj = snapshot.val();
+        for (var squareId in obj){
+            urlToNewCanvas(obj[squareId].url, squareId);
+        }
+
+    });
+
+
+    // ADD FRIENDS FUNCTIONALITY
+
+    $scope.showAllUsers = function() {
+        $scope.clicked = true;
+    };
+
+    $scope.addFriend = function(user) {
+        $scope.collabAdded = true;
+        $scope.collaborators.push(user);
+        user.collabr = true;
+        // $scope.searchedEmail = '';
+    };
+
+    $scope.removeCollabr = function(user) {
+        var userId = user._id;
+        var collabArr = [];
+        $scope.collaborators.forEach(function(collbr) {
+            collabArr.push(collbr._id);
+        });
+        var index = collabArr.indexOf(userId);
+        $scope.collaborators.splice(index, 1);
+        user.collabr = false;
+    };
+
+
+    $scope.addCollaborators = function() {
+        var collabsIds = $scope.collaborators.map(function(collabr) {
+            return collabr._id;
+        });
+
+        $scope.clicked = false;
+        $scope.collaborators = [];
+        $scope.collabAdded = false;
+
+        StoryFactory.addCollaborators($scope.story._id, collabsIds)
+        .then(function(updatedStory) {
+            console.log('Updated Story', updatedStory);
+        });
+    };
+
+
+
+});
+
